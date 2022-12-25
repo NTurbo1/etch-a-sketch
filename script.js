40cd77a0946435a22feb98945a09ad32f4fd3306
Nurbolat <nurbilim.kz@gmail.com>
2022-12-25 17:23:53 +0600

    In 'index.html':     Adds a div with id = 'buttons' inside which a button with     id = 'set-size'.
    In 'script.js':
        Adds an event listener to the button with id = 'set-size' for
        'click' event. The event listener creates an input with
        type = 'range', min = 0, max = 100 and inserts it after the
        button with id = 'set-size'.
    
        Adds an event listener to the previously created range input for
        'input' event. The event listener creates an output element with
        id = 'input-range-output' that displays the value of the range
        input and inserts it after the range input. If the div with
        id = 'buttons' already contains an output element with
        id = 'input-range-output' then the event listener only updates
        the text content of the output element.
    
        Defines a function called 'insertAfter' with 2 parameters to
        insert a node after a certain node.

--- script.js
+++ script.js
@@ -17,3 +17,29 @@ for (i = 1; i < 17; i++) {
     }
     gridContainer.appendChild(rowDiv);
 }
+
+const buttonsDiv = document.getElementById('buttons');
+const rangeInput = document.createElement('input');
+rangeInput.type = 'range';
+rangeInput.min = '0';
+rangeInput.max = '100';
+rangeInput.value = '50';
+
+rangeInput.addEventListener('input', (e) => {
+    let output = document.getElementById('input-range-output');
+    if (!buttonsDiv.contains(output)) {
+        output = document.createElement('output');
+        output.id = 'input-range-output';
+    }
+    output.textContent = e.target.value;
+    insertAfter(rangeInput, output);
+})
+
+const sizeButton = document.getElementById('set-size');
+sizeButton.addEventListener('click', () => {
+    insertAfter(sizeButton, rangeInput);
+})
+
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
