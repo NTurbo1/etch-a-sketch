953f6e6a33f9fbb8ce24ddd3e377e520e1aa9e86
Nurbolat <nurbilim.kz@gmail.com>
2022-12-25 18:41:54 +0600

    In 'script.js':     Changes 'rangeInput.min' from 0 to 1.
        Creates a button referenced by 'sizeSubmitButton'.
    
        Modifies the event listener of 'sizeButton' for 'click' event
        so that when 'sizeButton' is clicked it is removed and
        'sizeSubmitButton' is inserted before 'rangeInput'.
    
        Adds an event listener to 'sizeSubmitButton' for 'click' event.
        The event listener removes the current sketch pad and builds a new
        sketch pad with sideLength = 'rangeInput.value' in the same place.

--- script.js
+++ script.js
@@ -1,44 +1,64 @@
+const body = document.querySelector('body');
 const gridContainer = document.getElementById('grid-container');
 
-for (i = 1; i < 17; i++) {
-    const rowDiv = document.createElement('div');
-    rowDiv.id = 'row-' + i;
-    for (j = 1; j < 17; j++) {
-        const div = document.createElement('div');
-        div.id = 'row-' + i + '-col-' + j;
-        const originalBackgroundColor = div.style.backgroundColor;
-        div.addEventListener('mouseover', () => {
-            div.style.backgroundColor = 'orange';
-        });
-        div.addEventListener('mouseout', () => {
-            div.style.backgroundColor = originalBackgroundColor;
-        });
-        rowDiv.appendChild(div);
+function buildSketchPad(sideLength, sketchPadContainer) {
+    for (i = 1; i < sideLength; i++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.id = 'row-' + i;
+        for (j = 1; j < sideLength; j++) {
+            const div = document.createElement('div');
+            div.id = 'row-' + i + '-col-' + j;
+            const originalBackgroundColor = div.style.backgroundColor;
+            div.addEventListener('mouseover', () => {
+                div.style.backgroundColor = 'orange';
+            });
+            div.addEventListener('mouseout', () => {
+                div.style.backgroundColor = originalBackgroundColor;
+            });
+            rowDiv.appendChild(div);
+        }
+        sketchPadContainer.appendChild(rowDiv);
     }
-    gridContainer.appendChild(rowDiv);
 }
 
+buildSketchPad(16, gridContainer)//builds the initial sketchpad with sideLength = 16.
+
+const sizeButton = document.getElementById('set-size');
 const buttonsDiv = document.getElementById('buttons');
 const rangeInput = document.createElement('input');
 rangeInput.type = 'range';
-rangeInput.min = '0';
+rangeInput.min = '1';
 rangeInput.max = '100';
 rangeInput.value = '50';
 
+const rangeInputValueOutput = document.createElement('output');
+rangeInputValueOutput.textContent = rangeInput.value;
+rangeInputValueOutput.id = 'range-input-value-output';
+
 rangeInput.addEventListener('input', (e) => {
-    let output = document.getElementById('input-range-output');
-    if (!buttonsDiv.contains(output)) {
-        output = document.createElement('output');
-        output.id = 'input-range-output';
-    }
-    output.textContent = e.target.value;
-    insertAfter(rangeInput, output);
+    rangeInputValueOutput.textContent = e.target.value;
 })
 
-const sizeButton = document.getElementById('set-size');
+const sizeSubmitButton = document.createElement('button');
+sizeSubmitButton.id = 'set-submit-button';
+sizeSubmitButton.textContent = 'Submit';
+
 sizeButton.addEventListener('click', () => {
     insertAfter(sizeButton, rangeInput);
+    insertAfter(rangeInput, rangeInputValueOutput);
+    buttonsDiv.removeChild(sizeButton);
+    buttonsDiv.insertBefore(sizeSubmitButton, rangeInput);
 })
+
+let currentSketchPad = gridContainer;
+sizeSubmitButton.addEventListener('click', () => {
+    body.removeChild(currentSketchPad);
+    currentSketchPad = document.createElement('div');
+    currentSketchPad.id = 'grid-container';
+    body.appendChild(currentSketchPad);
+
+    buildSketchPad(rangeInput.value, currentSketchPad);
+});
 
 function insertAfter(referenceNode, newNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
